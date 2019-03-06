417f13a6b80d8588a62c8008f425ef5d97a88e5c
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-06 16:33:54 -0300

    Sample models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="ingredients", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
