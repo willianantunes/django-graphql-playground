c930ff39d3f232c4b73a68f6b9338c109816eab9
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-06 16:34:18 -0300

    In order to make all the models available in ADMIN interface

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.apps import apps
+from django.contrib import admin
+
+app = apps.get_app_config("api")
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
