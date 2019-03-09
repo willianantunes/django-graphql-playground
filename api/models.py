d619ff03cdd121306b0d624f3b3deae66734576c
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-09 18:22:21 -0300

    Updated model and makemigrations

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -11,9 +11,7 @@ class Category(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
-    category = models.ForeignKey(
-        Category, related_name="ingredients", on_delete=models.CASCADE
-    )
+    category = models.ForeignKey(Category, related_name="ingredients", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
