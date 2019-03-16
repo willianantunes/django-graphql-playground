436ef59ddacbdf44ba062ff208939da9364591ff
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-16 12:04:43 -0300

    New fields on each model
    `created` and `updated` timestamps

--- api/models.py
+++ api/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -12,6 +14,8 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
     category = models.ForeignKey(Category, related_name="ingredients", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
