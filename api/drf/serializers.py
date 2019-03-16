5cf01d065c0127590d733148392e36116d62ad01
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-16 12:44:19 -0300

    To make fields available for DRF and GraphQL

--- api/drf/serializers.py
+++ api/drf/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 
-from api.models import Category, Ingredient
+from api.models import Category
+from api.models import Ingredient
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ("id", "name")
+        fields = ("id", "name", "created_at", "updated_at")
 
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ("id", "name", "notes", "category")
+        fields = ("id", "name", "notes", "category", "created_at", "updated_at")
