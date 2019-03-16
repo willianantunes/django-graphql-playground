b72f0b0dd70ebd851afe30de6796ccb0e5fadd21
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-16 19:57:42 -0300

    Custom GraphQLView to make good use of DRF authentication setup

---
+++ api/graphql/views.py
@@ -0,0 +1,22 @@
+import rest_framework
+from graphene_django.views import GraphQLView
+from rest_framework.decorators import api_view
+from rest_framework.decorators import authentication_classes
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
+
+
+class DRFAuthenticatedGraphQLView(GraphQLView):
+    def parse_body(self, request):
+        if isinstance(request, rest_framework.request.Request):
+            return request.data
+        return super(DRFAuthenticatedGraphQLView, self).parse_body(request)
+
+    @classmethod
+    def as_view(cls, *args, **kwargs):
+        view = super(DRFAuthenticatedGraphQLView, cls).as_view(*args, **kwargs)
+        view = permission_classes((IsAuthenticated,))(view)
+        view = authentication_classes(api_settings.DEFAULT_AUTHENTICATION_CLASSES)(view)
+        view = api_view(["GET", "POST"])(view)
+        return view
