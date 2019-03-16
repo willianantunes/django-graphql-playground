ba33941c7389f65d0060cbab120cae8bffc276a7
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-16 15:09:03 -0300

    Index for created_at field on each model

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
     start_at = models.DateField(null=True)
     end_at = models.DateField(null=True)
@@ -16,7 +16,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
     category = models.ForeignKey(Category, related_name="ingredients", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
