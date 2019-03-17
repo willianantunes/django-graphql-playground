91e3ffc1a943326f2c948d0b8f403b1de3b81b96
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-17 10:43:24 -0300

    To generate the list of SQL queries

--- django_graphql_playground/settings.py
+++ django_graphql_playground/settings.py
@@ -137,3 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# Logging
+# https://docs.djangoproject.com/en/2.1/topics/logging/
+
+LOGGING = {
+    "version": 1,
+    "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler"}},
+    "loggers": {"django.db.backends": {"level": "DEBUG", "handlers": ["console"]}},
+}
