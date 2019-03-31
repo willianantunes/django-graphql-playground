4e5c51b1d6b547c8186b097b4e92fe38c26b4bca
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-31 18:50:47 -0300

    Updated scrip to run Django and .dockerignore file

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,7 +14,14 @@ if [ ${count} -eq 0 ] ; then
     echo "from django.contrib.auth import get_user_model;\
     User = get_user_model();\
     User.objects.create_superuser('admin', None, 'admin')" | python manage.py shell
-    echo "Done!"
 fi
 
-python manage.py runserver ${DJANGO_BIND_ADDRESS}:${DJANGO_BIND_PORT}
+
+if [[ ${DJANGO_BIND_ADDRESS+x} ]] && [[ ${DJANGO_BIND_PORT+x} ]];
+then
+    echo "OK! Using custom ADRESSS $DJANGO_BIND_ADDRESS and PORT $DJANGO_BIND_PORT to set Django runserver command"
+    python manage.py runserver ${DJANGO_BIND_ADDRESS}:${DJANGO_BIND_PORT}
+else
+    echo "Using 0.0.0.0:80 as parameter for Django runserver command"
+    python manage.py runserver 0.0.0.0:80
+fi
