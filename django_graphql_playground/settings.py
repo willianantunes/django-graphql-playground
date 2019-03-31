f8213fade76f7d4cb274416c170a7bd9f7e8655a
Willian Antunes <willian.lima.antunes@gmail.com>
2019-03-31 19:16:22 -0300

    WSGI only serves static if DEV is set as the ENVIRONMENT

--- django_graphql_playground/settings.py
+++ django_graphql_playground/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from enum import Enum
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,6 @@ LOGGING = {
         "django.db.backends": {"level": os.getenv("DJANGO_DB_BACKENDS_LOG_LEVEL", "INFO"), "handlers": ["console"]},
     },
 }
+
+Enviroments = Enum("Level", "DEV QA HOM STAGING PROD")
+ENVIRONMENT = os.getenv("ENVIRONMENT", Enviroments.DEV.name)
--- django_graphql_playground/wsgi.py
+++ django_graphql_playground/wsgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_graphql_playground.settings')
+from django_graphql_playground import settings
+from django_graphql_playground.settings import Enviroments
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_graphql_playground.settings")
 
 application = get_wsgi_application()
+
+
+if settings.ENVIRONMENT == Enviroments.DEV.name:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
